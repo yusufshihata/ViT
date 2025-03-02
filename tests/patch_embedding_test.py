79eff184decb7d7f4f7f44ff1c110a276665518c
yusufshihata <yusufshihata2006@gmail.com>
2025-03-02 11:17:59 +0200

    some testing

--- tests/patch_embedding_test.py
+++ tests/patch_embedding_test.py
@@ -4,14 +4,15 @@ from src.model import PatchEmbedding
 
 class PatchEmbeddingTest(unittest.TestCase):
     def test_forward(self):
+        batch_size = 10
         patch_res = 16
         img_shape = (3, 224, 224)
         patch_size = ((img_shape[1] * img_shape[2]) // patch_res**2) + 1
         latent_size = 512
-        img = torch.randn(1, *img_shape)
+        img = torch.randn(batch_size, *img_shape)
         patch_embedding = PatchEmbedding(patch_res, img_shape, latent_size)
         output = patch_embedding(img)
-        self.assertEqual(output.shape, torch.Size([1, patch_size, latent_size]))
+        self.assertEqual(output.shape, torch.Size([batch_size, patch_size, latent_size]))
 
 if __name__ == "__main__":
     unittest.main()
