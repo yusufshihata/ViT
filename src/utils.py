d752c648301776092d2aa35d62aefc02ad9de2b3
yusufshihata <yusufshihata2006@gmail.com>
2025-03-03 11:25:16 +0200

    checkpoint saver and loader

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,16 @@ import torch
 
 def calc_acc(num_pred: int, num_true: int) -> float:
     return num_pred / num_true
+
+def save_checkpoint(model: torch.nn.Module, optimizer: torch.optim.Optimizer, path: str):
+    checkpoint = {
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict()
+    }
+    torch.save(checkpoint, path)
+
+def load_checkpoint(model: torch.nn.Module, optimizer: torch.optim.Optimizer, path: str):
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint["model"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    return model, optimizer
